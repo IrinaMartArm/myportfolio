65330788eb690e6867c55485b524adb0efdf671f
Ирина Мартоян <surenira@icloud.com>
2024-03-08 19:49:57 +0400

    fix

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -5,9 +5,9 @@ import { animateScroll } from "react-scroll";
 export const Logo: React.FC = () => {
     
     return ( 
-        <a onClick={()=>{animateScroll.scrollToTop()}}>
+        <button onClick={()=>{animateScroll.scrollToTop()}}>
             <Icon iconId={'ira'}  width={"110"} height="18"/>
-        </a>
+        </button>
     );
 }
 
